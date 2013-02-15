79fd00c3095b4355d740c03ea683566b1b1bacc5
Anthony Scalisi <scalisi.a@gmail.com>
2013-02-15 13:58:19 -0800

    remove debugging output

--- fabtools/pkg.py
+++ fabtools/pkg.py
@@ -43,8 +43,7 @@ def is_installed(pkg_name):
     """
     Check if a package is installed.
     """
-#    with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
-    with settings(warn_only=True):
+    with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
         res = run("yes 'y' | pkgin list | egrep -qio \"^%(pkg_name)s\"" % locals())
         if res.succeeded:
             return True
